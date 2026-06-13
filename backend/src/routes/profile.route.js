29d3171a49d3f5179efaea5b4162c08c6d6b86bb
Tho <tranchitho160704@gmail.com>
2026-06-14 06:35:34 +0700

    Add profile API routes

---
+++ backend/src/routes/profile.route.js
@@ -0,0 +1,18 @@
+import express from "express";
+import {
+    getProfile,
+    updateMyProfile,
+    updateProfileMedia,
+} from "../controllers/profile.controller.js";
+import { protectRoute } from "../middlewares/auth.middleware.js";
+
+const router = express.Router();
+const handle = (controller) => (req, res, next) =>
+    Promise.resolve(controller(req, res, next)).catch(next);
+
+router.use(protectRoute);
+router.get("/:userId", handle(getProfile));
+router.patch("/me/details", handle(updateMyProfile));
+router.patch("/me/media/:field", handle(updateProfileMedia));
+
+export default router;
