7dca49c0c3202c97d31dbc26518ef9cdfc81616f
Tho <tranchitho160704@gmail.com>
2026-06-14 23:29:08 +0700

    Add post audience metadata helpers

--- frontend/src/components/PostCard.jsx
+++ frontend/src/components/PostCard.jsx
@@ -1,4 +1,4 @@
-import { MessageCircle, Send, Share2, ThumbsUp } from "lucide-react";
+import { Globe2, Lock, MessageCircle, Send, Share2, ThumbsUp, Users } from "lucide-react";
 import { useState } from "react";
 import { useAuthStore } from "../store/useAuthStore";
 import { useSocialStore } from "../store/useSocialStore";
@@ -7,6 +7,22 @@ const reactionOptions = [
   ["like", "👍"], ["love", "❤️"], ["haha", "😂"],
   ["wow", "😮"], ["sad", "😢"], ["angry", "😡"],
 ];
+
+const formatPostTime = (value) => new Intl.DateTimeFormat("vi-VN", {
+  hour: "2-digit",
+  minute: "2-digit",
+  second: "2-digit",
+  day: "2-digit",
+  month: "2-digit",
+  year: "numeric",
+  hour12: false,
+}).format(new Date(value));
+
+const audienceIcons = {
+  public: Globe2,
+  friends: Users,
+  private: Lock,
+};
 
 const PostCard = ({ post }) => {
   const authUser = useAuthStore((state) => state.authUser);
