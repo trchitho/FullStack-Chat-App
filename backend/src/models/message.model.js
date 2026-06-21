2b16791f146491d19fac6ed0647738deb56638a0
Tho <tranchitho160704@gmail.com>
2026-06-21 08:24:43 +0700

    feat(db): add query indexes to Message model

--- backend/src/models/message.model.js
+++ backend/src/models/message.model.js
@@ -71,6 +71,10 @@ const messageSchema = new mongoose.Schema(
     {timestamps: true}
 )
 
+messageSchema.index({ conversationId: 1, createdAt: -1 });
+messageSchema.index({ senderId: 1, receiverId: 1, createdAt: -1 });
+messageSchema.index({ receiverId: 1, "seenBy.user": 1 });
+
 const Message = mongoose.model('Message', messageSchema);
 
 export default Message;
